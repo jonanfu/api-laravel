621a436b4bc0a50790a0a93c76f1b2065e3b8bae
jonanfu <jonanfu@gmail.com>
2021-01-28 18:19:55 -0500

    Subida de archivos

---
+++ app/Http/Resources/V2/PostCollection.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\V2;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class PostCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public $collects = PostResource::class;
+
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection,
+            'meta' => [
+                'organization' => 'Platzi',
+                'authors' => [
+                    'Italomoralesf',
+                    'Platzi'
+                ]
+            ],
+            'type' => 'articles'
+        ];
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::apiResource('v1/posts', PostV1::class)
+    ->only(['index','show','destroy'])
+    ->middleware('auth:sanctum');
+
+Route::apiResource('v2/posts', PostV2::class)
+    ->only(['index','show'])
+    ->middleware('auth:sanctum');
+
+Route::post('login', [
+    App\Http\Controllers\Api\LoginController::class,
+    'login'
+]);
